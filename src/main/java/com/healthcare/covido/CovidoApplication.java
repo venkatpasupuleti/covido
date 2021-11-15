3bc13dc1f6c89e8d5f3a763932ee6a6114e72231
sriman <tech.sriman@gmail.com>
2021-11-15 10:06:11 +0530

    modified db

--- src/main/java/com/healthcare/covido/CovidoApplication.java
+++ src/main/java/com/healthcare/covido/CovidoApplication.java
@@ -2,14 +2,21 @@ package com.healthcare.covido;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class CovidoApplication implements WebMvcConfigurer {
+public class CovidoApplication extends SpringBootServletInitializer implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CovidoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CovidoApplication.class);
 	}
 
 	@Override
